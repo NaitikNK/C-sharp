f3cd1ed855cca70e409de4388d8094adba96ffe7
NaitikNK <naitikkapatel1@gmail.com>
2024-03-18 18:53:23 +0530

    Add project files.

---
+++ Trial 1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Trial_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            float j = 5;
+            String color = "red";
+
+            //type casting convert int to string
+            Console.WriteLine(i.GetType());
+
+            Console.WriteLine(Convert.ToString(i));
+            Console.WriteLine(Convert.ToString(i).GetType());
+
+            Console.WriteLine("\n");
+
+            //converting float to int
+            Console.WriteLine(j.GetType());
+
+            Console.WriteLine(Convert.ToInt32(j));
+            Console.WriteLine(Convert.ToInt32(j).GetType());
+
+
+
+        }
+    }
+}
