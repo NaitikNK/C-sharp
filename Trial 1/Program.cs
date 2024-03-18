86778d66d6120e2eb322b50c9a7ddea308199473
NaitikNK <naitikkapatel1@gmail.com>
2024-03-18 22:06:34 +0530

    commit

--- Trial 1/Program.cs
+++ Trial 1/Program.cs
@@ -1,9 +1,18 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Trial_1
 {
     class Program
     {
+        //Enum program value initialization
+        enum level
+        {
+            Tiger,
+            Bird,
+            Animal
+        }
+
         static void Main(string[] args)
         {
             int i = 10;
@@ -11,6 +20,7 @@ namespace Trial_1
             String color = "red";
 
             //type casting convert int to string
+            Console.WriteLine("Typecasting Program\n");
             Console.WriteLine(i.GetType());
 
             Console.WriteLine(Convert.ToString(i));
@@ -24,8 +34,41 @@ namespace Trial_1
             Console.WriteLine(Convert.ToInt32(j));
             Console.WriteLine(Convert.ToInt32(j).GetType());
 
+            Console.WriteLine("\n");
+
+            //Boxing Program 
+
+            int num = 100;
+
+            //boxing
+            //create object
+            object obj = num;
+
+            Console.WriteLine("Boxing Program");
+
+            //value of num change
+            num = 200;
+
+            Console.WriteLine(num);
+
+            Console.WriteLine("\n");
+            //Unboxing Program
+            int t = 100;
+
+            Console.WriteLine("Unboxing Program");
+
+            object obb = t;
+
+            Console.WriteLine(obb);
+            Console.WriteLine("\n");
+
+            //Enum Program
+            Console.WriteLine("Enum Program");
+            level h= level.Tiger;
+            Console.WriteLine(h);
 
 
         }
     }
 }
+
